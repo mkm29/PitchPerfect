0b1c3b9ee6ffa57df05e86c64aa4a8ac0e29a146
Mitch Murphy <mitch.murphy@gmail.com>
2016-04-01 10:33:56 -0700

    Cleaned up PlaySoundsViewController, removed all audio engine/playback functionality in preparation for PlaySoundsViewController extension

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -36,65 +36,15 @@ class PlaySoundsViewController: UIViewController {
             
         }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-    @IBAction func playSoundSlow(sender: UIButton) {
-        // its a good idea to always stop the audio player before playing and sounds
-        playSoundAtRate(0.5)
-    }
-
-    @IBAction func playSoundFast(sender: UIButton) {
-        // its a good idea to always stop the audio player before playing and sounds
-        playSoundAtRate(1.5)
-    }
-    
-    @IBAction func playChipmunkAudio(sender: UIButton) {
-        playChipmunkAudioWithVariablePitch(1000)
-    }
-    
-    @IBAction func playDarthVaderAudio(sender: UIButton) {
-        playChipmunkAudioWithVariablePitch(-1000)
-    }
-    
-    func playChipmunkAudioWithVariablePitch(pitch: Float) {
-        audioPlayer.stop()
-        audioEngine.stop()
-        audioEngine.reset()
-        
-        
-        let audioPlayerNode = AVAudioPlayerNode()
-        audioEngine.attachNode(audioPlayerNode)
-        
-        let changePitchEffect = AVAudioUnitTimePitch()
-        changePitchEffect.pitch = pitch
-        audioEngine.attachNode(changePitchEffect)
-        
-        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
-        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
-        
-        let audioFile = try! AVAudioFile(forReading: recordedAudio.filePath)
-        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
-        try! audioEngine.start()
-        audioPlayerNode.play()
+    @IBAction func playSoundFromButton(sender: UIButton) {
+        print("Play sound from button tapped")
     }
     
     @IBAction func stopAudio(sender: UIButton) {
         if audioPlayer.playing {
             audioPlayer.stop()
         }
-    }
-    
-    func playSoundAtRate(rate: Float) {
-        audioPlayer.stop()
-        audioEngine.stop()
-        audioEngine.reset()
-        audioPlayer.currentTime = 0.0
-        audioPlayer.rate = rate
-        audioPlayer.play()
     }
     
     /*
